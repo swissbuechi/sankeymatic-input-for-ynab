d1b56a8b5b2818578af34329ffd60fc6e5e15910
swissbuechi <raphael.buechi@hotmail.com>
2025-02-04 16:45:11 +0100

    Removed footer

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="navbar navbar-dark bg-primary d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow">
     <div class="container d-flex align-items-center">
-      <!-- Logo with link to home -->
       <a href="./" class="d-flex align-items-center text-decoration-none">
         <div
           style="
